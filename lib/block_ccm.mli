7995d2a8ab7ef84ca76ef18c6b24667c043bcdf6
Stefan Grundmann <sg2342@googlemail.com>
2015-08-06 19:38:25 +0000

    name argument key in connect

--- lib/block_ccm.mli
+++ lib/block_ccm.mli
@@ -5,6 +5,6 @@ module Make (B: V1.BLOCK
     with type 'a io = 'a Lwt.t
      and type page_aligned_buffer = Cstruct.t
 
-  val connect : ?maclen:int -> ?nonce_len:int -> Cstruct.t -> B.t ->
+  val connect : ?maclen:int -> ?nonce_len:int -> key:Cstruct.t -> B.t ->
     [`Ok of t | `Error of error] io
 end
